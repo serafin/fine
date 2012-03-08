d88b09eab90bf390f2d15debd3e6252656755020
Thomas Anderson <neoserafin@gmail.com>
2012-03-08 22:06:56 +0100

    f_v_helper_head update

--- src/lib/f/v/helper/head.php
+++ src/lib/f/v/helper/head.php
@@ -1,81 +1,174 @@
 <?php
 
-class f_c_helper_head
+class f_v_helper_head
 {
     
-    public $headTemplate  = array(
+    public $template = array(
+        'charset' => array(
+            'mode'      => 'item',
+            'template'  => "\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset={charset}\">\n",
+            'var'       => 'charset',
+        ),
+        'title' => array(
+            'mode'      => 'list',
+            'template'  => "{fragment}",
+            'var'       => 'fragment',
+            'prepend'   => "\t<title>",
+            'separator' => " | ",
+            'append'    => "</title>\n",
+        ),
+        'favicon' => array(
+            'mode'      => 'item',
+            'template'  => "\t<link href=\"/{href}\" rel=\"shortcut icon\">\n",
+            'var'       => 'href',
+        ),
+        'rss' => array(
+            'mode'      => 'list',
+            'template'  => "\t<link href=\"{href}\" title=\"{title}\" type=\"application/rss+xml\" rel=\"alternate\" {attr}/>\n",
+            'var'       => 'href',
+            'val'       => array('attr' => ''),
+        ),
+        'atom' => array(
+            'mode'      => 'list',
+            'template'  => "\n<link href=\"{href}\" title=\"{title}\" type=\"application/atom+xml\" rel=\"alternate\" {attr}/>",
+            'var'       => 'href',
+            'val'       => array('attr' => ''),
+        ),
         'css' => array(
             'mode'      => 'list',
-            'template'  => '<link rel="stylesheet" type="text/css" href="{href}" {attr}/>',
-            'var'       => '{href}',
-            'val'       => array('{attr}' => ''),
-            'prepend'   => "\t",
-            'separator' => "\n\t",
-            'append'    => "\n",
+            'template'  => "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"{href}\" {attr}/>\n",
+            'var'       => 'href',
+            'val'       => array('attr' => ''),
         ),
         'js' => array(
             'mode'      => 'list',
-            'template'  => '<script src="{src}" type="text/javascript"{attr}>{content}</script>',
-            'var'       => '{href}',
-            'val'       => array('{attr}' => '', '{content}' => ''),
-            'prepend'   => "\t",
-            'separator' => "\n\t",
-            'append'    => "\n",
+            'template'  => "\t<script src=\"{src}\" type=\"text/javascript\"{attr}>{content}</script>\n",
+            'var'       => 'href',
+            'val'       => array('attr' => '', 'content' => ''),
         ),
-        'jsInline' => array(/** @todo nazwe jakas porzadna ustawic */
+        'jscode' => array(
             'mode'      => 'list',
             'template'  => "{content}",
-            'var'       => '{content}',
+            'var'       => 'content',
             'prepend'   => "\t<script type=\"text/javascript\">\n",
             'separator' => "\n",
             'append'    => "\n</script>\n",
         ),
-        'cssInline' => array(/** @todo nazwe jakas porzadna ustawic */
+        'jsblock' => array(
+            'mode'      => 'list',
+            'template'  => "\t<script type=\"text/javascript\"{attr}>\n{content}\n\t</script>\n",
+            'var'       => 'content',
+            'val'       => array('attr' => ''),
+        ),
+        'csscode' => array(
             'mode'      => 'list',
             'template'  => "{content}",
-            'var'       => '{content}',
+            'var'       => 'content',
             'prepend'   => "\t<style type=\"text/css\">\n",
             'separator' => "\n",
             'append'    => "\n</style>\n",
         ),
-        'charset' => array(
-            'mode'      => 'item',
-            'template'  => "\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset={charset}\">\n",
-            'var'       => '{charset}',
-        ),
-        'favicon' => array(
-            'mode'      => 'item',
-            'template'  => "\t<link rel=\"shortcut icon\" href=\"/{href}\">\n",
-            'var'       => '{href}',
-        ),
-        'title' => array(
+        'cssblock' => array(
             'mode'      => 'list',
-            'template'  => "{title}",
-            'var'       => '{title}',
-            'prepend'   => "\t<title>",
-            'separator' => " | ",
-            'append'    => "</title>\n",
+            'template'  => "\t<style type=\"text/css\"{attr}>\n{content}\n\t</style>\n",
+            'var'       => 'content',
+            'val'       => array('attr' => ''),
         ),
         /** 
          * @todo
          *  - description
          *  - keywords
-         *  - language
-         *  - rss
-         *  - atom
-         *  - robots
-         *  - jsBlock
-         *  - cssBlock
          */
     );
     protected $_data = array();
     
     
-    public function helper(){} // render all
-    public function render($sType = null){} // render all or type
+    public static function _()
+    {
+        return new self;
+    }
 
-    // zarzadzanie danymi
-    public function head($sType, $asArg = null){}
-    public function remove();
+    public function __call($name, $arguments) 
+    {
+        $this->head($arguments, $arguments[0]);
+    }
+
+    public function helper()
+    {
+        return $this->render();
+    }
+    
+    public function render()
+    {
+        $output = '';
+        
+        foreach ($this->template as $type => $template) {
+            $output .= $this->renderType($type);
+        }
+        
+        return $output;
+    }
+    
+    public function renderType($sType)
+    {
+        $data = $this->_data[$sType];
+        $tpl  = $this->template[$sType];
+        
+        if (! isset($data)) {
+            return '';
+        }
+        
+        if ($tpl['mode'] == 'item') {
+            
+            $data    += (array)$tpl['val'];
+            $sOutput = $tpl['template'];
+            foreach ($data as $var => $val) {
+                $sOutput = str_replace("{{$var}}", htmlspecialchars($val), $sOutput);
+            }
+            
+            return $sOutput;
+            
+        }
+        else {
+            
+            $aOutput = array();
+            foreach ($data as $i) {
+                $i       += (array)$tpl['val'];
+                $sOutput = $tpl['template'];
+                foreach ($i as $var => $val) {
+                    $sOutput = str_replace("{{$var}}", htmlspecialchars($val), $sOutput);
+                }
+                $aOutput[] = $sOutput;
+            }
+            
+            return 
+                (string)$tpl['prepend'] 
+              . implode((string)$tpl['separator'], $aOutput) 
+              . (string)$tpl['append'];
+            
+        }
+        
+    }
+
+    public function head($sType, $asArg = null)
+    {
+        if (is_string($asArg)) {
+            $asArg = array($this->template[$sType]['var'] => $asArg);
+        }
+        
+        if ($this->template[$sType]['mode'] == 'item') {
+            $this->_data[$sType] = $asArg;
+        }
+        else {
+            $this->_data[$sType][] = $asArg;
+        }
+        
+    }
+    
+    public function remove($sType)
+    {
+        unset($this->_data[$sType]);
+    }
+        
     
 }
